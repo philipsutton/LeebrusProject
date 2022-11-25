b89d1a3fca5bee29e02ca15bdc4a76596e479b2f
Philip Sutton <s21038@pjwstk.edu.pl>
2022-11-25 18:59:17 +0100

    aaa

---
+++ MVCProject/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using MVCProject.Models;
+
+namespace MVCProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            //connect to db
+            SqlConnection con = new SqlConnection("Data Source=db-mssql.pjwstk.edu.pl;Initial Catalog=2019SBD;Integrated Security=True");
+            SqlCommand com = new SqlCommand();
+            com.Connection = con;
+            //fire query
+            com.CommandText = "select * from student";
+            con.Open();
+            SqlDataReader dr = com.ExecuteReader();
+
+            var students = new List<Student>();
+            while (dr.Read())
+            {
+                Student student = new Student();
+                student.FirstName = dr["FirstName"].ToString();
+                student.LastName = dr["LastName"].ToString();
+                student.Email = dr["Email"].ToString();
+                student.Address = dr["Address"].ToString();
+                students.Add(student);
+            }
+
+            ViewBag.Names = students;
+            ViewBag.Title = "Students";
+
+            return View(students);
+        }
+
+        public IActionResult Studies()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCProject.Models
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string  Email { get; set; }
+        public string Address { get; set; }
+        
+
+    }
+}
